0cc3b394743c85ee649786c39e81cfb0e5b2ae4c
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-22 20:10:51 -0500

    add validation to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+
+    validates :item_name, presence: true, length: { minimum: 2, maximum: 50 }
+    validates :category, presence: true, length: { minimum: 2, maximum: 50 }
+    validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
