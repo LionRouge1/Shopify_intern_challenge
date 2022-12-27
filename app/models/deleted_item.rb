31c150bef8efe74471cfa3bef266a12bc75a1c51
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-26 19:31:52 -0500

    add foreign_key reference to deleted item model

--- app/models/deleted_item.rb
+++ app/models/deleted_item.rb
@@ -1,5 +1,5 @@
 class DeletedItem < ApplicationRecord
-  belongs_to :item
+  belongs_to :item, class_name: 'Item', foreign_key: :item_id
 
   validates :description, presence: true, length: { minimum: 2, maximum: 250 }
 end
