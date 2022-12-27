70c66aca4b1f421abae2ef2332593bc7d782507f
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-26 19:29:42 -0500

    create deleted_items controller

--- app/controllers/deleted_items_controller.rb
+++ app/controllers/deleted_items_controller.rb
@@ -1,7 +1,37 @@
-class DeletedItemController < ApplicationController
+class DeletedItemsController < ApplicationController
+  before_action :set_deleted_item, only: %i[edit update destroy]
+  def index
+    @deleted_items = DeletedItem.includes(:item)
+  end
+
+  def edit; end
+
   def create; end
 
-  def update; end
+  def update
+    if @deleted_item.update(deleted_params)
+      redirect_to deleted_items_path, notice: 'Item was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    if(@deleted_item.destroy)
+      redirect_to deleted_items_path, notice: 'Item undeleted successfully'
+    else
+      flash.now[:alert] = 'Sorry something went wrong'
+      render :index
+    end
+  end
+
+  private
+
+  def set_deleted_item
+    @deleted_item = DeletedItem.find(params[:id])
+  end
+
+  def deleted_params
+    params.require(:deleted_item).permit(:description)
+  end
 end
