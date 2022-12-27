75c3ae49ec0be33732d8b371fca0d4fed10474a6
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-26 19:33:31 -0500

    define route for deleted_item requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   resources :items
+  get '/deleted_items/:id/edit', to: 'deleted_items#edit', as: 'edit_deleted'
+  get '/deleted_items', to: 'deleted_items#index'
+  patch '/deleted_items/:id', to: 'deleted_items#update', as: 'deleted'
+  post '/deleted_items/:id', to: 'deleted_items#create'
+  delete '/deleted_items/:id', to: 'deleted_items#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "pages#home"
+  root 'pages#home'
 end
