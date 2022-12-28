40857cde14fc3967ca439c7246b042d94866883e
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-27 19:42:08 -0500

    add validations to deletedItem model

--- app/models/deleted_item.rb
+++ app/models/deleted_item.rb
@@ -1,5 +1,6 @@
 class DeletedItem < ApplicationRecord
   belongs_to :item, class_name: 'Item', foreign_key: :item_id
 
-  validates :description, presence: true, length: { minimum: 2, maximum: 250 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 250 }
+  validates :item_id, uniqueness: { message: 'Item already deleted' }
 end
