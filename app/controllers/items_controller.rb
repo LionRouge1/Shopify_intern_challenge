8aa755a58d3833594ed024ca71dbdc3007f5fb68
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-31 17:47:07 -0500

    remove unused code from items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,27 +22,19 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to item_url(@item), notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      redirect_to items_path, notice: 'Item was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /items/1 or /items/1.json
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      redirect_to item_url(@item), notice: 'Item was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -50,10 +42,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
 
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to items_url, notice: 'Item was successfully destroyed.'
   end
 
   private
