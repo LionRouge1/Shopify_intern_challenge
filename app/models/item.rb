a93d9ccd362cbc8a0be82f63d49304b892396a83
Masoudi Parrot <masoudi@localhost.localdomain>
2023-01-03 21:46:14 -0500

    link item model with warehouse model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one :deleted_item, dependent: :destroy
+  has_many :items_warehouses, dependent: :destroy
 
   validates :item_name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :category, presence: true, length: { minimum: 2, maximum: 50 }
