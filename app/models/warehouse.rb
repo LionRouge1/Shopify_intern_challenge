e3fe9eb69c9ce5d9d2058c58d78078ccd77014d7
Masoudi Parrot <masoudi@localhost.localdomain>
2023-01-03 21:47:04 -0500

    link warehouse model with item model

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,2 +1,7 @@
 class Warehouse < ApplicationRecord
+  has_many :items_warehouses, dependent: :destroy
+
+  validates :address, presence: true, length: { minimum: 2, maximum: 150 }
+  validates :country, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :city, presence: true, length: { minimum: 2, maximum: 100 }
 end
