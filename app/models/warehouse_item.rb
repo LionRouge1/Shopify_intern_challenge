e6733bdc5eabd62f21fc97b101c5f9e6d9ff083b
Masoudi Parrot <masoudi@localhost.localdomain>
2023-01-07 11:25:08 -0500

    broadcasts warehouse_item

--- app/models/warehouse_item.rb
+++ app/models/warehouse_item.rb
@@ -7,10 +7,17 @@ class WarehouseItem < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit lambda {
-    broadcast_append_to 'warehouse_items',
-                        partilal: 'warehouse_items/warehouse_item',
-                        locals: { warehouse_item: self },
-                        target: 'warehouse_items'
-  }
+  # after_create_commit lambda {
+  #   broadcast_append_to 'warehouse_items',
+  #                       partilal: 'warehouse_items/warehouse_item',
+  #                       locals: { warehouse_item: self },
+  #                       target: 'warehouse_items'
+  # }
+
+  # after_create_commit -> { broadcast_append_to 'warehouse_items' }
+  # after_update_commit -> { broadcast_replace_to 'warehouse_items' }
+  # after_destroy_commit -> { broadcast_remove_to 'warehouse_items' }
+
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(warehouse_item) { 'warehouse_items' }, inserts_by: :prepend
 end
