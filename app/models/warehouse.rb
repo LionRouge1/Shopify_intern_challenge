41896d10157b76777e83e8d87803128af7f964a1
Masoudi Parrot <masoudi@localhost.localdomain>
2023-01-06 21:51:44 -0500

    change join table name to warehoure_items

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,5 @@
 class Warehouse < ApplicationRecord
-  has_many :items_warehouses, dependent: :destroy
+  has_many :warehouse_items, dependent: :destroy
 
   validates :address, presence: true, length: { minimum: 2, maximum: 150 }
   validates :country, presence: true, length: { minimum: 2, maximum: 100 }
