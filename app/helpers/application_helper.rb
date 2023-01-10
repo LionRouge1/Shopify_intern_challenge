50b5363e82dd42bf14fb667f8469ed6618b7b43e
Masoudi Parrot <masoudi@localhost.localdomain>
2023-01-10 17:05:47 -0500

    create ative_link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def active_link_to(link_text, link_path, link_class)
+    active = current_page?(link_path) ? 'active' : ''
+    link_to link_text, link_path, class: "#{link_class} #{active}"
+  end
 end
